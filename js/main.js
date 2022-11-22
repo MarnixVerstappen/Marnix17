08dc76c6e1bcc688edf7278510021834531b1db7
MarnixVerstappen <118810883+MarnixVerstappen@users.noreply.github.com>
2022-11-22 12:57:25 +0100

    alles

--- js/main.js
+++ js/main.js
@@ -1,58 +1,53 @@
 const boxes = document.querySelectorAll('.boxes');
 const playerX = 'X';
 const playerO = 'O';
-const boardState = Array(boxes.length); boardState.fill(null);
-const Strike = document.querySelector(".strike");
+const boardState = Array(boxes.length); boardState.fill('');
+const Strike = document.querySelector('.strike');
 const gameOverArea = document.querySelector('.game-over-area');
 const gameOverText = document.querySelector('.game-over-text');
 const playAgain = document.querySelector('.play-again');
-const winning = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9],
-    [1, 4, 7],
-    [2, 5, 8],
-    [3, 6, 9],
-    [1, 5, 9],
-    [3, 5, 7]
-]
 let turn = playerX;
-boxes.forEach(boxes => boxes.addEventListener("click", boxeClick));
 
+playAgain.addEventListener('click', reloads)
+function reloads() {
+    document.location.reloads()
+}
+
+boxes.forEach(boxes => boxes.addEventListener('click', boxeClick));
 boxes.forEach(function(box) { 
-    box.addEventListener("mouseover", function() {
+    box.addEventListener('mouseover', function() {
         const hoverClass = `${turn.toLowerCase()}-hover`;
 
         this.classList.add(hoverClass);
     });
-    box.addEventListener("mouseout", function() {
-        this.classList.remove("x-hover");
-        this.classList.remove("o-hover");
+    box.addEventListener('mouseout', function() {
+        this.classList.remove('x-hover');
+        this.classList.remove('o-hover');
     });
 
 });
 
 function setHoverText() {
     boxes.forEach((boxes) => {
-        boxes.classList.remove("X-hover");
-        boxes.classList.remove("O-hover");
+        boxes.classList.remove('X-hover');
+        boxes.classList.remove('O-hover');
     });
 
 
     boxes.forEach((boxes) => {
-        if (boxes.innerText == "") {
+        if (boxes.innerText == '') {
            boxes.classList.add(hoverClass);
         }
     });
 }
 
 function boxeClick(event) {
-    if (gameOverArea.classList.contains("visible")) {
+    if (gameOverArea.classList.contains('visible')) {
         return;
     }
     const boxes = event.target;
     const boxNumber = boxes.dataset.index;
-    if(boxes.innerText != "") {
+    if(boxes.innerText != '') {
         return;
     }
 
@@ -66,35 +61,38 @@ function boxeClick(event) {
         boardState[boxNumber-1] = playerO
         turn = playerX;
     }   
+    console.log(checkWinnerCombination);
+    console.log(turn);
+    if (checkWinner() == turn) {
+        gameOverText.innerHTML = `${turn} has won`
+    }
     checkWinner();
-    // console.log(boardState);
 }
 
 function checkWinner() {
-    checkWinnerCombination.forEach((combination,index) => {
+    checkWinnerCombination.forEach((combination) => {
         const indexes = combination.combo;
         const useClass = combination.strikeClass;
-        if (boardState[indexes[0]] != null && 
+        if (boardState[indexes[0]] != '' && 
             boardState[indexes[0]] === boardState[indexes[1]] && 
             boardState[indexes[1]] === boardState[indexes[2]]) {
                 console.log('winner');
+                console.log(useClass);
         }
+        return checkWinnerCombination;
     })
- //   console.log(boardState)
+   console.log(boardState);
 }
 
 let checkWinnerCombination = [
-    // rows
-    { combo: [0, 1, 2], strikeClass: ".strike-row-1" },
-    { combo: [3, 4, 5], strikeClass: ".strike-row-2" },
-    { combo: [6, 7, 8], strikeClass: ".strike-row-3" },
-    //colums
-    { combo: [0, 3, 6], strikeClass: ".strike-colum-1" },
-    { combo: [1, 4, 7], strikeClass: ".strike-colum-2" },
-    { combo: [2, 5, 8], strikeClass: ".strike-colum-3" },
-    //diagonals
-    { combo: [0, 4, 8], strikeClass: ".strike-diagonal-1" },
-    { combo: [2, 4, 6], strikeClass: ".strike-diagnoal-2" },
-//    0
+    { combo: [0, 1, 2], strikeClass: gameOverText.innerHTML = `${turn} has won` },
+    { combo: [3, 4, 5], strikeClass: gameOverText.innerHTML = `${turn} has won` },
+    { combo: [6, 7, 8], strikeClass: gameOverText.innerHTML = `${turn} has won` },
+    { combo: [0, 3, 6], strikeClass: gameOverText.innerHTML = `${turn} has won` },
+    { combo: [1, 4, 7], strikeClass: gameOverText.innerHTML = `${turn} has won` },
+    { combo: [2, 5, 8], strikeClass: gameOverText.innerHTML = `${turn} has won` },
+    { combo: [0, 4, 8], strikeClass: gameOverText.innerHTML = `${turn} has won` },
+    { combo: [2, 4, 6], strikeClass: gameOverText.innerHTML = `${turn} has won` },
 ]
+
 
